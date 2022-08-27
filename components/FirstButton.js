67f934549d807e13963f0bc8f180db69441b3113
Sidharth Raj <sidharth-raj@outlook.com>
2022-08-28 01:16:06 +0530

    storybook setup

---
+++ components/FirstButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstButton() {
+    return <button>Press me!</button>;
+}
+
+export default FirstButton;
---
+++ stories/components/Home/home.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FirstButton from "../../../components/FirstButton";
+
+export default {
+    title: "FirstButton",
+    component: FirstButton,
+};
+
+export const HomePage = () => <FirstButton />;
