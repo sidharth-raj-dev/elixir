9b06dffe5c83550dc0576b783d4ee2f1d6d997b9
Sidharth Raj <sidharth-raj@outlook.com>
2022-08-28 15:13:35 +0530

    added card styles

--- components/card/index.js
+++ components/card/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./css/card.module.css";
 
-function Card() {
-    return <div className={styles.card}></div>;
+function Card({styleConfig}) {
+    return <div className={styles.card} style={{
+        height: styleConfig.height,
+        width: styleConfig.width,
+        borderRadius: styleConfig.border_radius,
+        background: styleConfig.background
+    }}></div>;
 }
 
 export default Card;
--- stories/components/Card/card.stories.jsx
+++ stories/components/Card/card.stories.jsx
@@ -6,4 +6,16 @@ export default {
     component: Card,
 };
 
-export const DefaultCard = () => <Card />;
+const Template = (args) => <Card {...args} />;
+
+export const Dynamic = Template.bind({});
+
+const config = {
+    height: "250px",
+    width: "250px",
+    border_radius: "50px",
+    background: "#e0e0e0"
+};
+Dynamic.args = { styleConfig: config };
+
+export const Default = () => <Card styleConfig={config}/>;
